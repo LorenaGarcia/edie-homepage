c11ecdc0bc059afcbaf98f1020ffe5a92c294fe5
LorenaGarcia <lorenagarciar34@gmail.com>
2021-02-02 18:29:51 -0600

    Updates

---
+++ precache-manifest.a8a32540161fb11c9b42e1b3eb11d06c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01b10c832b40e60fc76684d8c0b9dc71",
+    "url": "/edie-homepage/index.html"
+  },
+  {
+    "revision": "88019c415dc6a58d4ab2",
+    "url": "/edie-homepage/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9926d1097092a829ee87",
+    "url": "/edie-homepage/static/js/2.261a084f.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/edie-homepage/static/js/2.261a084f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88019c415dc6a58d4ab2",
+    "url": "/edie-homepage/static/js/main.19c7f625.chunk.js"
+  },
+  {
+    "revision": "7af32c1f696dc53feba8",
+    "url": "/edie-homepage/static/js/runtime-main.41610ff4.js"
+  },
+  {
+    "revision": "d16aefb36765a1816db146553bd7fa53",
+    "url": "/edie-homepage/static/media/booking.d16aefb3.png"
+  },
+  {
+    "revision": "d628474129cdcd112a82e83bcfc3f75f",
+    "url": "/edie-homepage/static/media/heroImage.d6284741.jpg"
+  },
+  {
+    "revision": "18af0d3854701bdb82702d712a3dc3da",
+    "url": "/edie-homepage/static/media/instagram.18af0d38.svg"
+  },
+  {
+    "revision": "096e56b14be7c99b3c5b9b364a83a9aa",
+    "url": "/edie-homepage/static/media/juice-product.096e56b1.png"
+  },
+  {
+    "revision": "4052ea9c1b03b201835185b0bbb09c74",
+    "url": "/edie-homepage/static/media/linkedin.4052ea9c.svg"
+  },
+  {
+    "revision": "64ae8cffbacef95b029b0132e7996bef",
+    "url": "/edie-homepage/static/media/onboard.64ae8cff.png"
+  },
+  {
+    "revision": "444a54425ac84b0061d938a22f979e38",
+    "url": "/edie-homepage/static/media/person1.444a5442.png"
+  },
+  {
+    "revision": "cab0dff601d1747bb42a5e6588148af2",
+    "url": "/edie-homepage/static/media/person2.cab0dff6.png"
+  },
+  {
+    "revision": "c28533f26985cc237d1257bc4cc6ea59",
+    "url": "/edie-homepage/static/media/person3.c28533f2.png"
+  },
+  {
+    "revision": "b42c6136e516e378deebc09a9b87cd67",
+    "url": "/edie-homepage/static/media/person4.b42c6136.png"
+  },
+  {
+    "revision": "e0886190511336577acef1f653707818",
+    "url": "/edie-homepage/static/media/smarthome.e0886190.jpg"
+  },
+  {
+    "revision": "5710b3e19d0ca09dbc13903bbdac3b8b",
+    "url": "/edie-homepage/static/media/twitter.5710b3e1.svg"
+  }
+]);
